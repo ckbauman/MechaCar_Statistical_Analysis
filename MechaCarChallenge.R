d0e6fa3c2d985a1e342c97b93835fa2c543ae4c3
Kathy Bauman <kathybmans@gmail.com>
2021-10-09 00:18:26 -0500

    adding images and modified R file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -25,7 +25,7 @@ head(coil_table)
 
 # 3. Write an RScript that creates a total_summary dataframe using the summarize()
 # function to get the mean, median, variance, and standard deviation of the suspension
-# coil's PSI column
+# coil's PSI column (section 15.2.5)
 
 # import tidyverse library
 library(tidyverse)
@@ -36,6 +36,36 @@ total_summary <- coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Vari
 # 4. Write an RScript that creates a lot_summary dataframe using the group_by and the summarize() functions to
 # group each mfg lot by the mean, median, variance and std dev of the PSI column.
 lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),StDev=sd(PSI), .groups = 'keep')
+
+
+# Deliverable 3
+# 1. Write an RScript using the t.test() function to determine if the PSI across all mfg lots is statistically
+# different from the population mean of 1,500 PSI (section 15.6.2)
+t.test(coil_table$PSI,mu=1500)
+
+# 2. write 3 more RScripts using t.test() and its subset() to determine if PSI for each mfg lot is
+# statistically different from pop mean of 1,500 PSI
+# Lot 1
+t.test(subset(coil_table,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+# Lot 2
+t.test(subset(coil_table,Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+# Lot 3
+t.test(subset(coil_table,Manufacturing_Lot=="Lot3")$PSI,mu=1500)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -15,6 +15,37 @@ INSERT: linear image
 - Is the slope of the linear model considered to be zero? Why or why not?
 - Does this linear model predict mpg of MechaCar prototypes effectively? why or why not?
 
+### Summary Statistics on Suspension Coils (Deliverable 2)
+
+Total_Summary Results:
+
+INSERT: total sum image
+
+Lot_Summary Results:
+
+INSERT lot sum image
+
+- design specs for mechaCar suspension coils dictate that the variance of the suspension coils must not exceed 100 PSI.
+
+Does the current mfg data meet this design spec for all mfg lots in total and each lot individualy? why or why not?
+
+### T-Tests on Suspension Coils (Deliverable 3)
+
+PSI results for ALL mfg lots compared to population mean of 1,500 PSI:
+
+INSERT all ttest image
+
+PSI results for Lot 1 compared to mean of 1,500:
+
+INSERT lot1 ttest image
+
+PSI results for Lot 2 compared to mean of 1,500:
+
+INSERT lot2 ttest image
+
+PSI results for Lot 3 compared to mean of 1,500:
+
+INSERT lot3 ttest image
 
 
 ## Summary
